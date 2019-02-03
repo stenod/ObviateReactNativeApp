51ad863a7ebc9302d0aff8a576ac94ec7896e636
Stephan <s0544659@htw-berlin.de>
2019-02-03 21:30:06 +0100

    - added charts

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -147,33 +147,33 @@ export default class HomeScreen extends React.Component {
 
 
 
-                <View style={styles.getStartedContainer}>
-                    {this._maybeRenderDevelopmentModeWarning()}
+                {/*<View style={styles.getStartedContainer}>*/}
+                    {/*{this._maybeRenderDevelopmentModeWarning()}*/}
 
-                    <Text style={styles.getStartedText}>Get started by opening</Text>
+                    {/*<Text style={styles.getStartedText}>Get started by opening</Text>*/}
 
-                    <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-                        <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-                    </View>
+                    {/*<View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>*/}
+                        {/*<MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>*/}
+                    {/*</View>*/}
 
-                    <Text style={styles.getStartedText}>
-                        Healthy Me macht voll Spaß
-                    </Text>
-                </View>
+                    {/*<Text style={styles.getStartedText}>*/}
+                        {/*Healthy Me macht voll Spaß*/}
+                    {/*</Text>*/}
+                {/*</View>*/}
 
-                <View style={styles.helpContainer}>
-                    <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-                        <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-                    </TouchableOpacity>
-                </View>
+                {/*<View style={styles.helpContainer}>*/}
+                    {/*<TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>*/}
+                        {/*<Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>*/}
+                    {/*</TouchableOpacity>*/}
+                {/*</View>*/}
 
-                <View style={styles.tabBarInfoContainer}>
-                    <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
+                {/*<View style={styles.tabBarInfoContainer}>*/}
+                    {/*<Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>*/}
 
-                    <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-                        <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-                    </View>
-                </View>
+                    {/*<View style={[styles.codeHighlightContainer, styles.navigationFilename]}>*/}
+                        {/*<MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>*/}
+                    {/*</View>*/}
+                {/*</View>*/}
             </ScrollView>
 
         )
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,7 +1,31 @@
 import React from 'react';
-import {Button, Image, ScrollView, StyleSheet, View} from 'react-native';
+import {Button, Image, ScrollView, StyleSheet, View, Text} from 'react-native';
 import {ExpoLinksView} from '@expo/samples';
 import {API, Auth} from "aws-amplify";
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph
+} from 'react-native-chart-kit'
+import { Dimensions } from 'react-native'
+const screenWidth = Dimensions.get('window').width
+
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [{
+        data: [ 20, 45, 28, 80, 99, 43 ],
+        color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})` // optional
+    }]
+};
+
+const chartConfig = {
+    backgroundGradientFrom: '#1E2923',
+    backgroundGradientTo: '#08130D',
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`
+};
 
 export default class LinksScreen extends React.Component {
     static navigationOptions = {
@@ -32,13 +56,50 @@ export default class LinksScreen extends React.Component {
             <ScrollView style={styles.container}>
                 {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
-                <View style={styles.card}>
-                    <View style={{marginLeft: 15, alignSelf: 'flex-start'}}>
-                        <Image style={{width: 50, height: 50, resizeMode: 'contain', alignSelf: 'stretch'}} source={require('../assets/images/purple.png')} />
-                    </View>
-                    <View style={{ width: 120, alignSelf: 'flex-start'}}>
-                        {/*<Button color="#000" title="Stimmung" style={styles.bold} onPress={}/>*/}
-                    </View>
+                <View>
+                    <Text>
+                        Bezier Line Chart
+                    </Text>
+                    <LineChart
+                        data={{
+                            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+                            datasets: [{
+                                data: [
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100
+                                ]
+                            }]
+                        }}
+                        width={Dimensions.get('window').width} // from react-native
+                        height={220}
+                        chartConfig={{
+                            backgroundColor: '#e26a00',
+                            backgroundGradientFrom: '#fb8c00',
+                            backgroundGradientTo: '#ffa726',
+                            decimalPlaces: 2, // optional, defaults to 2dp
+                            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                            style: {
+                                borderRadius: 16
+                            }
+                        }}
+                        bezier
+                        style={{
+                            marginVertical: 8,
+                            borderRadius: 16
+                        }}
+                    />
+                </View>
+                <View>
+                    <LineChart
+                        data={data}
+                        width={screenWidth}
+                        height={220}
+                        chartConfig={chartConfig}
+                    />
                 </View>
             </ScrollView>
         );
